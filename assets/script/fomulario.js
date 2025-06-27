f586c7435751af537c932300dc24e068fca57c92
Juan Rodriguez <satanasdelcolo@gmail.com>
2025-06-26 22:58:10 -0400

    Validacion de campos completados del fomulario y se eliminan espacios en blanco al inicio y al final de cada campo

--- assets/script/fomulario.js
+++ assets/script/fomulario.js
@@ -1,6 +1,6 @@
 const formulario = document.querySelector('#fomulario');
 
-formulario.addEventListener('submit', envioForm)
+formulario.addEventListener('submit', envioForm);
 
 function limpiarFomulario() {
 
@@ -10,18 +10,40 @@ function limpiarFomulario() {
 
 }
 
+function validarFormulario(nombre, email, mensaje){
+    if(nombre == "" || email == "" || mensaje == "") {
+        // Notificación con Toastify
+        Toastify({
+            text: "Debes rellenar todos los campos",
+            duration: 3000,  
+            close: true,     
+            gravity: "top",  
+            position: "right", 
+            backgroundColor: "linear-gradient(to right,rgb(176, 0, 9),rgb(201, 61, 131))", 
+            stopOnFocus: true, 
+        }).showToast();
+        return false;
+    }
+    return true;
+};
+
+
 function envioForm(e){
-    e.preventDefault()
+    e.preventDefault();
+    
+    const nombre = document.querySelector('#idNombre').value.trim();
+    const email = document.querySelector('#idEmail').value.trim();
+    const mensaje = document.querySelector('#idMensaje').value.trim();
+    
+    if(!validarFormulario(nombre, email, mensaje)){
+        return;
+    };
 
-    const nombre = document.querySelector('#idNombre').value;
-    const email = document.querySelector('#idEmail').value;
-    const mensaje = document.querySelector('#idMensaje').value;
-
-    console.log(`${nombre}, ${email}, ${mensaje}`);
+    console.log(`'${nombre}', ${email}, ${mensaje}`);
 
     // Notificación con Toastify
     Toastify({
-        text: `Gracias ${nombre}, formulario enviado correctamente!`,
+        text: `Gracias, ${nombre}, formulario enviado correctamente!`,
         duration: 3000,  
         close: true,     
         gravity: "top",  
@@ -31,7 +53,7 @@ function envioForm(e){
     }).showToast();
 
     // Limpiar formulario luego de enviar
-    limpiarFomulario()
+    limpiarFomulario();
 }
 
 envioForm();
